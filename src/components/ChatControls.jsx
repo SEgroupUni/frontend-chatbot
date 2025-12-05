4f3e85116b8967d7ac661f23d396053cfdfdb4a1
Dom056 <dominicdawson99@gmail.com>
2025-12-05 15:00:47 +0000

    End session button linked to backend

--- src/components/ChatControls.jsx
+++ src/components/ChatControls.jsx
@@ -1,6 +1,23 @@
 import "./ChatPage.css";
 
 export default function ChatControls() {
+
+  const endSession = async () => {
+    try {
+      await fetch("http://localhost:3001/api/sessionEnd", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" }
+      });
+
+      // Once backend confirms redirect to Home page
+      window.location.href = "/";
+    } catch (err) {
+      console.error("Error ending session:", err);
+      // If there is an error still redirect but backend will not save log
+      window.location.href = "/";
+    }
+  };
+
   return (
     <div className="chat-box-controls">
 
@@ -13,7 +30,7 @@ export default function ChatControls() {
 
       <button
         className="chat-box-end"
-        onClick={() => (window.location.href = "/")} // end session post
+        onClick={endSession}
       >
         End Session
       </button>
